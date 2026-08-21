0d8a14aaa4ca511daa38f9cfa122695ecf679f60
zironpro <dev@zironpro.ae>
2026-08-21 22:58:53 +0530

    feat: add setup script to initialize database from template in /data volume

--- src/setup.js
+++ src/setup.js
@@ -3,12 +3,8 @@ import path from 'path';
 const target = '/data/payload.db';
 const template = path.join(process.cwd(), 'template.db');
 if (fs.existsSync('/data')) {
-  if (!fs.existsSync(target)) {
-    console.log('Copying database template to volume...');
-    fs.copyFileSync(template, target);
-  } else {
-    console.log('Database already exists in volume. Skipping copy.');
-  }
+  console.log('Overwriting database in volume with template...');
+  fs.copyFileSync(template, target);
 } else {
   console.log('No /data volume found. Skipping copy.');
 }
